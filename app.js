2c18df6ce894dea0eaaa82fb419946d76a697c69
squidhawkman <steveswansonium@gmail.com>
2022-08-03 13:15:46 +0900

    Added slider, did some refactoring, etc.

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
+//some variables
 const generatedPokemonContainer = document.querySelector('#generatedPokemonContainer');
-const baseURL = 'Sprites/'
+const baseURL = 'ClassicSprites/'
 const baseURLshiny = 'Shinies/'
-
-//listing the pokemon in order
-// for (let i = 1; i <= 200; i++) {
-//     const newImg = document.createElement('img');
-//     newImg.src = `${baseURL}${i}.png`;
-//     generatedPokemonContainer.append(newImg);
-// }
-
-
-//default is gens I-IV
-function generateRandPokeNum(fromPokeNum = 1, toPokeNum = 493, type) {
-    return Math.floor(Math.random() * toPokeNum + fromPokeNum);
-}
 
 
 //Gen I 1-151
@@ -21,17 +9,40 @@ function generateRandPokeNum(fromPokeNum = 1, toPokeNum = 493, type) {
 //Gen III 252-386
 //Gen IV 387-493
 
+//default is gens I-IV
+function generateRandPokeNum(fromPokeNum = 1, toPokeNum = 493, type) {
+    return Math.floor(Math.random() * toPokeNum + fromPokeNum);
+}
+
+
 //keeping track of what pokemon have been generated (so there are not repeats)
 let generatedPokemon = [];
+
 
 //disabling save team button until team is full
 const saveTeamButton = document.querySelector('#saveTeam');
 saveTeamButton.disabled = true;
 
-//having headers hidden while empty
+
+//having header hidden while empty
 const newTeamHeader = document.querySelector('#newTeamHeader');
 const savedTeamsHeader = document.querySelector('#savedTeamsHeader');
 savedTeamsHeader.style.visibility = 'hidden';
+
+//the slider 
+const chooseFromSlider = document.querySelector('#slider');
+const sliderDisplayNum = document.querySelector('#sliderDisplayNum');
+let pokemansNumber = chooseFromSlider.value;
+sliderDisplayNum.innerText = chooseFromSlider.value;
+chooseFromSlider.addEventListener('input', function() {
+    sliderDisplayNum.innerText = this.value;
+    pokemansNumber = this.value;
+})
+chooseFromSlider.addEventListener('change', function() {
+    fullReset();
+    generatePokemon();
+})
+
 
 
 //object that allows user to customize team picker
@@ -39,11 +50,15 @@ savedTeamsHeader.style.visibility = 'hidden';
 
 const customizer = {
     Gens: ['GenI', 'GenII', 'GenIII', 'GenIV'],
-    //Types: ['water', 'fire'], (list all types as default)
+    Types: ['water', 'fire', 'normal', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug', 'ghost', 'steel', 'dragon', 'dark', 'fairy'],
     Shiny: false
 }
 
-//buttons for the customizer
+
+//the Pokemon (I will get their data by fetching it from PokeAPI, James Q Quick has a guide on this that you favorited)
+
+
+//Activating shinies button
 const shinyButton = document.querySelector('#shinyButton');
 shinyButton.addEventListener('click', function () {
     if (this.innerText === 'Shinies?') {
@@ -66,7 +81,7 @@ newTeamContainer.append(newTeamList);
 //random Pokemon generator
 function generatePokemon() {
     //generating the Pokemon
-    for (let i = 1; i <= 4; i++) {
+    for (let i = 1; i <= pokemansNumber; i++) {
         let randPokeImg = document.createElement('img');
         //giving each sprite a click event that puts them on the new team list and clears the container of generated pokemon
         randPokeImg.addEventListener('click', function () {
@@ -107,6 +122,20 @@ function generatePokemon() {
 //generate first set
 generatePokemon();
 
+//reset functions
+function resetButSaveTeams() {
+    newTeamList.innerHTML = '';
+    generatedPokemon = [];
+    newTeamHeader.innerText = 'Build a team?';
+}
+
+function fullReset() {
+    generatedPokemonContainer.innerHTML = '';
+    newTeamList.innerHTML = '';
+    generatedPokemon = [];
+    newTeamHeader.innerText = 'Build a team?';
+}
+
 //saving the team
 //could include option to name team
 const savedTeamsContainer = document.querySelector('#savedTeamsContainer');
@@ -116,20 +145,15 @@ saveTeamButton.addEventListener('click', function() {
         newSavedTeam.append(listItem.querySelector('img'));
     }
     savedTeamsContainer.append(newSavedTeam);
-    newTeamList.innerHTML = '';
-    generatedPokemon = [];
     savedTeamsHeader.style.visibility = 'visible';
-    newTeamHeader.innerText = 'Build a team?';
+    resetButSaveTeams();
     generatePokemon();
 });
 
 //scrapping the team and making a new one
 const newTeamButton = document.querySelector('#clearTeam');
 newTeamButton.addEventListener('click', function() {
-    generatedPokemonContainer.innerHTML = '';
-    newTeamList.innerHTML = '';
-    generatedPokemon = [];
-    newTeamHeader.innerText = 'Build a team?';
+    fullReset();
     generatePokemon();
 });
 
